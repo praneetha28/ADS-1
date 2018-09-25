32db1429e6d2f99b585703f3c5406c3d988d6d42
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-09-25 18:02:05 +0530

    "check_style_score -> 24 of 24 passed. style score: 0/1 "

--- ADS-1 assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS-1 assignments/m2/CodeCamp_Percolation/Solution.java
@@ -1,15 +1,17 @@
 // public class Percolation {
-//    public Percolation(int n)                // create n-by-n grid, with all sites blocked
-//    public    void open(int row, int col)    // open site (row, col) if it is not open already
-//    public boolean isOpen(int row, int col)  // is site (row, col) open?
-//    public boolean isFull(int row, int col)  // is site (row, col) full?
-//    public     int numberOfOpenSites()       // number of open sites
-//    public boolean percolates()              // does the system percolate?
+//    public Percolation(int n)
+// create n-by-n grid, with all sites blocked
+//    public    void open(int row, int col)
+// open site (row, col) if it is not open already
+//    public boolean isOpen(int row, int col)
+// is site (row, col) open?
+//    public boolean isFull(int row, int col)
+// is site (row, col) full?
+//    public     int numberOfOpenSites()
+// number of open sites
+//    public boolean percolates()
+// does the system percolate?
 // }
-/**.
- * { item_description }
- */
-import java.io.BufferedInputStream;
 /**.
  * { item_description }
  */
@@ -17,16 +19,29 @@ import java.util.Scanner;
 /**.
  * { item_description }
  */
-
 public final class Solution {
-	public static void main(String[] args) {
+	/**.
+	 * Constructs the object.
+	 */
+	private Solution() {
+		/**.
+		 * { item_description }
+		 */
+	}
+	/**.
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
 		Percolation p = new Percolation(n);
 		while (sc.hasNextLine()) {
 			String line = sc.nextLine();
 			String[] tokens = line.split(" ");
-			p.open(Integer.parseInt(tokens[0]) - 1,Integer.parseInt(tokens[1]) - 1);
+			p.open(Integer.parseInt(tokens[0]) - 1,
+				Integer.parseInt(tokens[1]) - 1);
 		}
 		System.out.println(p.percolates());
 	}
