b57ab70d855db1d9b46149ae63d83791d714cd83
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-09-25 18:03:10 +0530

    "check_style_score -> 24 of 24 passed. style score: 1/1 "

--- ADS-1 assignments/m2/CodeCamp_Percolation/Solution.java
+++ ADS-1 assignments/m2/CodeCamp_Percolation/Solution.java
@@ -20,29 +20,29 @@ import java.util.Scanner;
  * { item_description }
  */
 public final class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	private Solution() {
-		/**.
-		 * { item_description }
-		 */
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		Percolation p = new Percolation(n);
-		while (sc.hasNextLine()) {
-			String line = sc.nextLine();
-			String[] tokens = line.split(" ");
-			p.open(Integer.parseInt(tokens[0]) - 1,
-				Integer.parseInt(tokens[1]) - 1);
-		}
-		System.out.println(p.percolates());
-	}
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        Percolation p = new Percolation(n);
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            String[] tokens = line.split(" ");
+            p.open(Integer.parseInt(tokens[0]) - 1,
+                Integer.parseInt(tokens[1]) - 1);
+        }
+        System.out.println(p.percolates());
+    }
 }
