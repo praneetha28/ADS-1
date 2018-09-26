ab1de9296e6f3b6ab9546c7bc7ecafed49b47dee
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-09-26 19:51:18 +0530

    "check_style_score -> 2 of 3 passed. style score: 0/1 "

--- ADS-1 assignments/Module -3/m3/Sorted_Array/Solution.java
+++ ADS-1 assignments/Module -3/m3/Sorted_Array/Solution.java
@@ -25,20 +25,20 @@ public final class Solution {
      */
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        // System.out.println("kdvn");
-        // int[] a = {10,15,22,80};
-        // int[] b = {5,8,11,15,70,90};
         int size = sc.nextInt();
         int size1 = sc.nextInt();
         int[] a = new int[size];
         int[] b = new int[size1];
-
         int[] c = new int[size + size1];
+        String line = sc.next();
+        String[] tokens = line.split(",");
         for (int n = 0; n < size; n++) {
-            a[n] = sc.nextInt();
+            a[n] = Integer.parseInt(tokens[n]);
         }
+        String line1 = sc.next();
+        String[] tokens1 = line1.split(",");
         for (int n = 0; n < size1; n++) {
-            b[n] = sc.nextInt();
+            b[n] = Integer.parseInt(tokens1[n]);
         }
         int i = 0;
         int j = 0;
