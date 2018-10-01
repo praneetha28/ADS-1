86e1d431184725ad5a2dac82f7b5afab26e8a3d3
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-01 11:28:12 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS-1 assignments/m7/7.1 Steque/Solution.java
+++ ADS-1 assignments/m7/7.1 Steque/Solution.java
@@ -2,10 +2,23 @@
  * { item_description }
  */
 import java.util.Scanner;
+/**.
+ * { item_description }
+ */
 public final class Solution {
+	/**.
+	 * Constructs the object.
+	 */
 	private Solution() {
-
+		/**.
+		 * { item_description }
+		 */
 	}
+	/**.
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
@@ -32,6 +45,8 @@ public final class Solution {
 					case "enqueue":
 						stq.enqueue(Integer.parseInt(tokens[1]));
 						System.out.println(stq.display());
+						break;
+					default:
 						break;
 				}
 				if (tokens[0].equals("")) {
