f6ebeab62e8c885f297cccaea808557041931f08
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-01 11:16:07 +0530

    "check_style_score -> 0 of 2 passed. style score: 0/1 "

--- ADS-1 assignments/m7/7.1 Steque/Solution.java
+++ ADS-1 assignments/m7/7.1 Steque/Solution.java
@@ -10,8 +10,8 @@ public final class Solution {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
 		for (int i = 0; i < n; i++) {
-			Steque stq = new Steque();
 			while (sc.hasNextLine()) {
+				Steque stq = new Steque();
 				String line = sc.nextLine();
 				String[] tokens = line.split(" ");
 				switch (tokens[0]) {
