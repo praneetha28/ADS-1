d255b5df4f453de8723313ad2572bd76d02a14c4
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-01 11:31:12 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS-1 assignments/m7/7.1 Steque/Solution.java
+++ ADS-1 assignments/m7/7.1 Steque/Solution.java
@@ -6,54 +6,52 @@ import java.util.Scanner;
  * { item_description }
  */
 public final class Solution {
-	/**.
-	 * Constructs the object.
-	 */
-	private Solution() {
-		/**.
-		 * { item_description }
-		 */
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		for (int i = 0; i < n; i++) {
-			Steque stq = new Steque();
-			while (sc.hasNextLine()) {
-				String line = sc.nextLine();
-				String[] tokens = line.split(" ");
-				switch (tokens[0]) {
-					case "push":
-						stq.push(Integer.parseInt(tokens[1]));
-						System.out.println(stq.display());
-						break;
-					case "pop":
-						stq.pop();
-						if (stq.isEmpty()) {
-							// System.out.println("empty");
-							System.out.println("Steque is empty.");
-						} else {
-							// System.out.println("not empty");
-							System.out.println(stq.display());
-						}
-						break;
-					case "enqueue":
-						stq.enqueue(Integer.parseInt(tokens[1]));
-						System.out.println(stq.display());
-						break;
-					default:
-						break;
-				}
-				if (tokens[0].equals("")) {
-					System.out.println();
-					break;
-				}
-			}
-		}
-	}
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < n; i++) {
+            Steque stq = new Steque();
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                String[] tokens = line.split(" ");
+                switch (tokens[0]) {
+                    case "push":
+    stq.push(Integer.parseInt(tokens[1]));
+                        System.out.println(stq.display());
+                        break;
+                    case "pop":
+                        stq.pop();
+                        if (stq.isEmpty()) {
+                            System.out.println("Steque is empty.");
+                        } else {
+                            System.out.println(stq.display());
+                        }
+                        break;
+                    case "enqueue":
+                        stq.enqueue(Integer.parseInt(tokens[1]));
+                        System.out.println(stq.display());
+                        break;
+                    default:
+                        break;
+                }
+                if (tokens[0].equals("")) {
+                    System.out.println();
+                    break;
+                }
+            }
+        }
+    }
 }
