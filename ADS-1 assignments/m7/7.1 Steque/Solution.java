5062b6083c38127973e4b05d0ed352612426ace2
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-01 11:29:16 +0530

    "check_style_score -> 2 of 2 passed. style score: 0/1 "

--- ADS-1 assignments/m7/7.1 Steque/Solution.java
+++ ADS-1 assignments/m7/7.1 Steque/Solution.java
@@ -19,7 +19,7 @@ public final class Solution {
 	 *
 	 * @param      args  The arguments
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int n = Integer.parseInt(sc.nextLine());
 		for (int i = 0; i < n; i++) {
