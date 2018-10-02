ae175f7fb6ae369ee45a6ab48f06c304c8c446d3
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-02 18:09:38 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- ADS-1 assignments/m8/8.1 Team Ranking With Insertion/Solution.java
+++ ADS-1 assignments/m8/8.1 Team Ranking With Insertion/Solution.java
@@ -1,25 +1,41 @@
+/**.
+ * { item_description }
+ */
 import java.util.Scanner;
+/**.
+ * { item_description }
+ */
 public final class Solution {
-	private Solution() {
-
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		Sorting sort = new Sorting();
-		// int i = 0;
-		while(sc.hasNext()) {
-			String line = sc.nextLine();
-			String[] tokens = line.split(",");
-			Team team = new Team(tokens[0], Integer.parseInt(tokens[1]),
-			 Integer.parseInt(tokens[2]), Integer.parseInt(tokens[3]));
-			// System.out.println(tokens[0]);
-			// System.out.println(Integer.parseInt(tokens[1]));
-			sort.add(team);
-			// i++;
-		}
-		// System.out.println("sort");
-		sort.selection();
-		// System.out.println("print");
-		System.out.println(sort.toString());
-	}
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Sorting sort = new Sorting();
+        // int i = 0;
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
+            String[] tokens = line.split(",");
+            Team team = new Team(tokens[0], Integer.parseInt(tokens[1]),
+             Integer.parseInt(tokens[2]), Integer.parseInt(tokens[2 + 1]));
+            // System.out.println(tokens[0]);
+            // System.out.println(Integer.parseInt(tokens[1]));
+            sort.add(team);
+            // i++;
+        }
+        // System.out.println("sort");
+        sort.selection();
+        // System.out.println("print");
+        System.out.println(sort.toString());
+    }
 }
