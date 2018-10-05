7384556b4529ba5f6c998a6cc37a1248ca5a7172
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-05 19:28:25 +0530

    "ads1_m11_a1_v2: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-1 assignments/m11/11.1 Cutoff Quicksort to Insertion Sort - updated/Sorting.java
@@ -0,0 +1,84 @@
+/**.
+ * Class for sorting.
+ */
+class Sorting {
+	// Comparable[] arr;
+	Sorting() {
+        /**.
+         * { item_description }
+         */
+	}
+// time complexity for this method is O(N^2).
+// In first for loop and second loop it iterates through the size of array
+	public void insertion(final Comparable[] arr, final int low, final int high) {
+		// int i = low;
+		// int j = high;
+		for (int i = low; i <= high; i++) {
+    		for (int j = i; j > low && less(arr[j], arr[j - 1]); j--) {
+             	swap(arr, j, j-1);
+    		}
+		}
+	}
+// time complexity of this method is O(1). It swaps the elements only once.
+	public void swap(final Comparable[] arr, final int j, final int min) {
+		Comparable temp = arr[j];
+	    arr[j] = arr[min];
+	    arr[min] = temp;
+	}
+// time complexity of this method is O(N). It iterates through
+// the array to print all the objects in array.
+	public String toString(final Comparable[] arr) {
+		// System.out.println("in print");
+		String str = "[";
+		for (int i = 0; i < arr.length - 1; i++) {
+			str += arr[i] + ", ";
+		}
+		str += arr[arr.length - 1] + "]";
+		return str;
+	}
+// time complexity is O(N)
+	public void sort(final Comparable[] arr, final int low, final int high, final int cutOff)
+    {
+        if (high <= low + cutOff - 1) {
+        	insertion(arr, low, high);
+        	System.out.println("insertionSort called");
+        	return;
+        }
+        int k = partition(arr, low, high);
+        System.out.println(toString(arr));
+        sort(arr, low, k - 1, cutOff);
+        sort(arr, k + 1, high, cutOff);
+    }
+//time complexity is O(1)
+    public void sort(final Comparable[] array, final int cutOff) {
+    	sort(array, 0, array.length - 1, cutOff);
+    }
+// time complexity is O(N)
+    public int partition(final Comparable[] arr, final int low, final int high) {
+        int i = low;
+        int j = high + 1;
+        while (true) {
+            while (less(arr[++i], arr[low])) {
+                if (i == high) {
+                    break;
+                }
+            }
+            while (less(arr[low], arr[--j])) {
+                if (j == low) {
+                    break;
+                }
+            }
+            if (i >= j) {
+                break;
+            }
+            swap(arr, i, j);
+        }
+        swap(arr, low, j);
+        // System.out.println(toString(arr));
+        return j;
+    }
+// time complexity is O(1)
+    public boolean less(Comparable a, Comparable b) {
+        return a.compareTo(b)<0;
+    }
+}
