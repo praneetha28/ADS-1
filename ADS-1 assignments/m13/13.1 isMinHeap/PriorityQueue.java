382592f4d881f7b5c972b7bc68cf0d0f60ccd9f4
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-08 15:53:09 +0530

    "ads1_m13_a1: 2/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ADS-1 assignments/m13/13.1 isMinHeap/PriorityQueue.java
@@ -0,0 +1,48 @@
+/**.
+ * { item_description }
+ */
+import java.util.Arrays;
+class PriorityQueue {
+	Comparable[] arr;
+    int size;
+	PriorityQueue() {
+        arr = new Comparable[10];
+        size = 0;
+	}
+// time complexity for this method is O(N^2).
+// In first for loop and second loop it iterates through the size of array
+	public boolean insert(Comparable[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+            add(arr[i]);
+        }
+        if (isMinHeap(1)) {
+            return true;
+        } else {
+            return false;
+        }
+	}
+    public void add(Comparable k) {
+        if (size == arr.length - 1) {
+            resize();
+        }
+        arr[++size] = k;
+    }
+    public void resize() {
+        arr = Arrays.copyOf(arr, arr.length * 2);
+    }
+    public boolean isMinHeap(int k) {
+        if (k > size) {
+            return true;
+        }
+        if (2 * k <= size && greater(k, 2 * k)) {
+            return false;
+        }
+        if (2 * k + 1 <= size && greater(k, 2 * k + 1)) {
+            return false;
+        }
+        return isMinHeap(2 * k) && isMinHeap(2 * k + 1);
+    }
+    public boolean greater(int a, int b) {
+        return arr[a].compareTo(arr[b]) > 0;
+    }
+}
---
+++ ADS-1 assignments/m13/13.1 isMinHeap/Solution.java
@@ -0,0 +1,41 @@
+/**.
+ * { item_description }
+ */
+import java.util.Scanner;
+/**.
+ * { item_description }
+ */
+public final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    // time complexity for the main method is N
+    // Because there is one while loop.
+    // while loop iterates until it has next line i.e N times.
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PriorityQueue pq = new PriorityQueue();
+        String type = sc.nextLine();
+        int testcases = sc.nextInt();
+        sc.nextLine();
+        for (int i = 0; i < testcases; i++) {
+            String line = sc.nextLine();
+            String[] tokens = line.split(",");
+            if (tokens[0].equals("")) {
+                System.out.println("false");
+            } else {
+                System.out.println(pq.insert(tokens));
+            }
+        }
+    }
+}
