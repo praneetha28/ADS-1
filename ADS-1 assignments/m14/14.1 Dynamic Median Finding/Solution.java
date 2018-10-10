9b816d299475eb6c9f991c8fabe851868d707d94
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-10 17:55:11 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/m14/14.1 Dynamic Median Finding/Solution.java
+++ ADS-1 assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -1,31 +1,51 @@
-/**.
- * { item_description }
- */
 import java.util.Scanner;
-/**.
- * { item_description }
- */
-public final class Solution {
-    /**.
-     * Constructs the object.
-     */
-    private Solution() {
-        /**.
-         * { item_description }
-         */
+public class Solution {
+
+    private Solution () {
+
     }
-    /**.
-     * { function_description }
-     *
-     * @param      args  The arguments
-     */
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        Median median = new Median(n);
+
+        MinPQ<Float> minpq = new MinPQ<Float>(n);
+        MaxPQ<Float> maxpq = new MaxPQ<Float>(n);
+
+        Float median = 0.0f;
         for (int i = 0; i < n; i++) {
-        	Float val = scan.nextFloat();
-        	median.insert(val);
-    	}
+            Float val = scan.nextFloat();
+            if (val > median) {
+                minpq.insert(val);
+            } else {
+                maxpq.insert(val);
+            }
+
+            if (minpq.size() - maxpq.size() > 1) {
+                maxpq.insert(minpq.delMin());
+
+            }
+            if (maxpq.size() - minpq.size() > 1) {
+                minpq.insert(maxpq.delMax());
+
+            }
+
+            if (minpq.size() == maxpq.size()) {
+                median = (minpq.min() + maxpq.max()) / 2;
+                System.out.println(median);
+            }
+
+            if (maxpq.size() > minpq.size()) {
+                median = maxpq.max();
+                System.out.println(median);
+            }
+
+            if (minpq.size() > maxpq.size()) {
+                median = minpq.min();
+                System.out.println(median);
+            }
+
+        }
+
     }
 }
