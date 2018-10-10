a6fbad22f5299636a33ba73bfae335510035a837
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-10 17:53:33 +0530

    "ads1_m14_a1: 1/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/m14/14.1 Dynamic Median Finding/Median.java
+++ ADS-1 assignments/m14/14.1 Dynamic Median Finding/Median.java
@@ -1,35 +1,41 @@
 class Median {
-	private MaxPQ<Float> max;
-    private MinPQ<Float> min;
-    Median() {
-    	max = new MaxPQ<Float>();
-    	min = new MinPQ<Float>();
+	private MaxPQ<Float> maxpq;
+    private MinPQ<Float> minpq;
+    Median(int n) {
+    	maxpq = new MaxPQ<Float>(n);
+    	minpq = new MinPQ<Float>(n);
     }
     public void insert(float k) {
     	Float median = 0.0f;
-        if (k > median) {
-            min.insert(k);
-        } else {
-            max.insert(k);
-        }
-        if (min.size() - max.size() > 1) {
-            max.insert(min.delMin());
-        }
-        if (max.size() - min.size() > 1) {
-            min.insert(max.delMax());
-        }
-        if (max.size() == min.size()) {
-            median = (min.min() + max.max()) / 2;
-            System.out.println(median);
-        }
-        if (max.size() > min.size()) {
-            median = max.max();
-            System.out.println(median);
-        }
-        if (min.size() > max.size()) {
-            median = min.min();
-            System.out.println(median);
-        }
+            if (k > median) {
+                minpq.insert(k);
+            } else {
+                maxpq.insert(k);
+            }
+
+            if (minpq.size() - maxpq.size() > 1) {
+                maxpq.insert(minpq.delMin());
+
+            }
+            if (maxpq.size() - minpq.size() > 1) {
+                minpq.insert(maxpq.delMax());
+
+            }
+
+            if (minpq.size() == maxpq.size()) {
+                median = (minpq.min() + maxpq.max()) / 2;
+                System.out.println(median);
+            }
+
+            if (maxpq.size() > minpq.size()) {
+                median = maxpq.max();
+                System.out.println(median);
+            }
+
+            if (minpq.size() > maxpq.size()) {
+                median = minpq.min();
+                System.out.println(median);
+            }
     }
 
 }
--- ADS-1 assignments/m14/14.1 Dynamic Median Finding/Solution.java
+++ ADS-1 assignments/m14/14.1 Dynamic Median Finding/Solution.java
@@ -1,51 +1,31 @@
+/**.
+ * { item_description }
+ */
 import java.util.Scanner;
-public class Solution {
-
-    private Solution () {
-
+/**.
+ * { item_description }
+ */
+public final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
     }
-
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-
-        MinPQ<Float> minpq = new MinPQ<Float>(n);
-        MaxPQ<Float> maxpq = new MaxPQ<Float>(n);
-
-        Float median = 0.0f;
+        Median median = new Median(n);
         for (int i = 0; i < n; i++) {
-            Float val = scan.nextFloat();
-            if (val > median) {
-                minpq.insert(val);
-            } else {
-                maxpq.insert(val);
-            }
-
-            if (minpq.size() - maxpq.size() > 1) {
-                maxpq.insert(minpq.delMin());
-
-            }
-            if (maxpq.size() - minpq.size() > 1) {
-                minpq.insert(maxpq.delMax());
-
-            }
-
-            if (minpq.size() == maxpq.size()) {
-                median = (minpq.min() + maxpq.max()) / 2;
-                System.out.println(median);
-            }
-
-            if (maxpq.size() > minpq.size()) {
-                median = maxpq.max();
-                System.out.println(median);
-            }
-
-            if (minpq.size() > maxpq.size()) {
-                median = minpq.min();
-                System.out.println(median);
-            }
-
-        }
-
+        	Float val = scan.nextFloat();
+        	median.insert(val);
+    	}
     }
 }
