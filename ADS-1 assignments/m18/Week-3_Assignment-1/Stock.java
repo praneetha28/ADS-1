c688978bcc79cad3e8a6ff5a582f31abd5fdb938
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-13 13:10:41 +0530

    "ads1_week3_exam: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1 assignments/m18/Week-3_Assignment-1/Stock.java
+++ ADS-1 assignments/m18/Week-3_Assignment-1/Stock.java
@@ -2,66 +2,66 @@
  * Class for stock.
  */
 class Stock {
-	/**.
-	 * { var_description }
-	 */
-	String name;
-	/**.
-	 * { var_description }
-	 */
-	Float value;
-	/**.
-	 * Constructs the object.
-	 *
-	 * @param      name   The name
-	 * @param      value  The value
-	 */
-	Stock(final String name, final Float value) {
-		this.name = name;
-		this.value = value;
-	}
-	/**.
-	 * Gets the name.
-	 *
-	 * @return     The name.
-	 */
-	public String getName() {
-		return this.name;
-	}
-	/**.
-	 * Gets the value.
-	 *
-	 * @return     The value.
-	 */
-	public Float getValue() {
-		return this.value;
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      that  The that
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public int compareTo(final Stock that) {
-		if (this.getValue() > that.getValue()) {
-			return 1;
-		} else if (this.getValue() < that.getValue()) {
-			return -1;
-		} else if (this.getName().compareTo(that.getName()) > 0) {
-			return 1;
-		} else if (this.getName().compareTo(that.getName()) < 0) {
-			return -1;
-		} else {
-			return 0;
-		}
-	}
-	/**.
-	 * Returns a string representation of the object.
-	 *
-	 * @return     String representation of the object.
-	 */
-	public String toString() {
-		return this.getName() + " " + this.getValue();
-	}
+    /**.
+     * { var_description }
+     */
+    private String name;
+    /**.
+     * { var_description }
+     */
+    private Float value;
+    /**.
+     * Constructs the object.
+     *
+     * @param      nme   The name
+     * @param      vlue  The value
+     */
+    Stock(final String nme, final Float vlue) {
+        this.name = nme;
+        this.value = vlue;
+    }
+    /**.
+     * Gets the name.
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return this.name;
+    }
+    /**.
+     * Gets the value.
+     *
+     * @return     The value.
+     */
+    public Float getValue() {
+        return this.value;
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Stock that) {
+        if (this.getValue() > that.getValue()) {
+            return 1;
+        } else if (this.getValue() < that.getValue()) {
+            return -1;
+        } else if (this.getName().compareTo(that.getName()) > 0) {
+            return 1;
+        } else if (this.getName().compareTo(that.getName()) < 0) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+    /**.
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        return this.getName() + " " + this.getValue();
+    }
 }
