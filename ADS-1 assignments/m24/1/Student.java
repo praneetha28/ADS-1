f618cec90087c65428e44153d94598c30d5d3992
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-27 11:19:48 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-1 assignments/m24/1/Student.java
+++ ADS-1 assignments/m24/1/Student.java
@@ -1,16 +1,46 @@
+/**.
+ * Class for student.
+ */
 class Student {
-	String sname;
-	double marks;
-	Student(String name, double m) {
+	/**.
+	 * { var_description }
+	 */
+	private String sname;
+	/**.
+	 * { var_description }
+	 */
+	private double marks;
+	/**.
+	 * Constructs the object.
+	 *
+	 * @param      name  The name
+	 * @param      m     { parameter_description }
+	 */
+	Student(final String name, final double m) {
 		this.sname = name;
 		this.marks = m;
 	}
+	/**.
+	 * Gets the name.
+	 *
+	 * @return     The name.
+	 */
 	public String getName() {
 		return this.sname;
 	}
+	/**.
+	 * Gets the marks.
+	 *
+	 * @return     The marks.
+	 */
 	public double getMarks() {
 		return this.marks;
 	}
+	/**.
+	 * Returns a string representation of the object.
+	 *
+	 * @return     String representation of the object.
+	 */
 	public String toString() {
 		return this.getName() + "," + this.getMarks();
 	}
