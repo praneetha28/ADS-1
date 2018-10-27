fd8896e274e507ea4dc9ee7f5e1cc8b8b9b4338d
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-27 11:20:50 +0530

    "m24_a1_1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-1 assignments/m24/1/Student.java
+++ ADS-1 assignments/m24/1/Student.java
@@ -2,46 +2,46 @@
  * Class for student.
  */
 class Student {
-	/**.
-	 * { var_description }
-	 */
-	private String sname;
-	/**.
-	 * { var_description }
-	 */
-	private double marks;
-	/**.
-	 * Constructs the object.
-	 *
-	 * @param      name  The name
-	 * @param      m     { parameter_description }
-	 */
-	Student(final String name, final double m) {
-		this.sname = name;
-		this.marks = m;
-	}
-	/**.
-	 * Gets the name.
-	 *
-	 * @return     The name.
-	 */
-	public String getName() {
-		return this.sname;
-	}
-	/**.
-	 * Gets the marks.
-	 *
-	 * @return     The marks.
-	 */
-	public double getMarks() {
-		return this.marks;
-	}
-	/**.
-	 * Returns a string representation of the object.
-	 *
-	 * @return     String representation of the object.
-	 */
-	public String toString() {
-		return this.getName() + "," + this.getMarks();
-	}
+    /**.
+     * { var_description }
+     */
+    private String sname;
+    /**.
+     * { var_description }
+     */
+    private double marks;
+    /**.
+     * Constructs the object.
+     *
+     * @param      name  The name
+     * @param      m     { parameter_description }
+     */
+    Student(final String name, final double m) {
+        this.sname = name;
+        this.marks = m;
+    }
+    /**.
+     * Gets the name.
+     *
+     * @return     The name.
+     */
+    public String getName() {
+        return this.sname;
+    }
+    /**.
+     * Gets the marks.
+     *
+     * @return     The marks.
+     */
+    public double getMarks() {
+        return this.marks;
+    }
+    /**.
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        return this.getName() + "," + this.getMarks();
+    }
 }
